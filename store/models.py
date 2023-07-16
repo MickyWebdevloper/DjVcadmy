104832d1052ada399d137685d2c47eec76d98e7e
Rofik <virusmicky132@gmail.com>
2023-07-16 11:52:29 +0530

    Added sqldb

--- store/models.py
+++ store/models.py
@@ -1,34 +1,40 @@
-from  django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.db import models
 
 User = get_user_model()
 
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_active=True)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = "Categories"
 
     def get_absolute_url(self):
         return reverse("store:category_list", args=[self.slug])
 
     def __str__(self):
         return self.name
-    
+
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='product',on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creater')
+    category = models.ForeignKey(
+        Category, related_name="product", on_delete=models.CASCADE
+    )
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="product_creater"
+    )
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255, default='admin')
+    author = models.CharField(max_length=255, default="admin")
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='images/default.jpg')
+    image = models.ImageField(upload_to="images/", default="images/default.jpg")
     slug = models.SlugField(max_length=255, unique=True)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
@@ -37,8 +43,8 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'Products'
-        ordering = ('-created',)
+        verbose_name_plural = "Products"
+        ordering = ("-created",)
 
     def get_absolute_url(self):
         return reverse("store:product_detail", args=[self.slug])
@@ -46,5 +52,5 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    # products = ProductManager()
+    products = ProductManager()
     objects = models.Manager()
--- store/views.py
+++ store/views.py
@@ -3,17 +3,52 @@ from .models import Product, Category
 
 
 def products_all(request):
-    products = Product.objects.all()[:3]
-    # products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
+    """
+    Retrieve all active products and render them on the index page.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        A rendered response containing all active products.
+    """
+    # products = Product.objects.all()[:5]
+    products = Product.products.all()
+    return render(request, "store/home.html", {"products": products})
 
 
 def product_detail(request, slug):
+    """
+    Retrieve details of a specific product by its slug and render them on the single product page.
+
+    Args:
+        request: The HTTP request object.
+        slug (str): The slug of the product.
+
+    Returns:
+        A rendered response containing the details of the specified product.
+    """
+
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/single.html', {'product': product})
+    return render(request, "store/products/single.html", {"product": product})
 
 
 def category_list(request, category_slug):
+    """
+    Retrieve products belonging to a specific category and render them on the category page.
+
+    Args:
+        request: The HTTP request object.
+        category_slug (str, optional): The slug of the category to filter products by. Defaults to None.
+
+    Returns:
+        A rendered response containing the specified category and its associated products.
+    """
+
     category = get_object_or_404(Category, slug=category_slug)
     product = Product.objects.filter(category=category)
-    return render(request, 'store/products/category.html', {'category': category, 'product': product})
+    return render(
+        request,
+        "store/products/category.html",
+        {"category": category, "product": product},
+    )
